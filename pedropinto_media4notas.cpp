246ecfec848b6cf2db6c71ab5de598113432283c
Paws55 <phmp222@gmail.com>
2023-10-16 12:36:58 +0100

    alterações ao calculo da média

--- pedropinto_media4notas.cpp
+++ pedropinto_media4notas.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main() {
     vector<float> notas(4);
     float media = 0;
+    float media_ponderada = 0;
 
     for (int i = 0; i < notas.size(); i++) {
-        cout << "Insira a nota " << i + 1 << ": ";
+        cout << "Inserir a nota " << i + 1 << ": ";
         cin >> notas[i];
         media += notas[i];
     }
 
     media /= notas.size();
+
+    media_ponderada = (notas[0] * 0.5) + (notas[1] * 0.1) + (notas[2] * 0.1) + (notas[3] * 0.3);
 
     cout << "Notas inseridas: ";
     for (int i = 0; i < notas.size(); i++) {
@@ -20,7 +23,8 @@ int main() {
     }
     cout << endl;
 
-    cout << "Média das notas == " << media;
+    cout << "Média == " << media << endl;
+    cout << "Média ponderada == " << media_ponderada;
 
     return 0;
 }
